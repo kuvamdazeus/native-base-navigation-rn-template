c2a1618cf721f6df1a81d416bf9ddef40c562563
Kuvam Bhardwaj <kuvamyash@gmail.com>
2022-07-25 23:14:36 +0530

    yeah, what?

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { NativeBaseProvider } from "native-base";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
--- screens/Home/index.tsx
+++ screens/Home/index.tsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React from "react";
 import { Box, Text } from "native-base";
 
 export default function Home() {

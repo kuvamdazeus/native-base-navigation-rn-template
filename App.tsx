4f44eaeb68f1a882aecad2a8d7af90d44078c2e4
Kuvam Bhardwaj <kuvamyash@gmail.com>
2022-07-25 23:02:34 +0530

    added necessary files

--- App.tsx
+++ App.tsx
@@ -1,20 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from "react";
+import { NativeBaseProvider } from "native-base";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Home from "./screens/Home";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { Box, Text } from "native-base";
+
+export default function Home() {
+  return (
+    <Box w="full" h="full" display="flex" bg="amber.200" justifyContent="center" alignItems="center">
+      <Text>Hello World!</Text>
+    </Box>
+  );
+}
